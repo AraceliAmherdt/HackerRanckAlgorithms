021528bc3df8bcd0aeca4bcdc8be7090e50e581a
AraceliAmherdt <114768217+AraceliAmherdt@users.noreply.github.com>
2023-05-05 17:13:09 -0300

    Update SherlockAndValidStringBuffer.java

--- SherlockAndValidStringBuffer.java
+++ SherlockAndValidStringBuffer.java
@@ -20,24 +20,24 @@ class Result {
      */
 
     public static String isValid(String s) {
-     int[] alphabet = new int['z' - 'a' + 1]; // Se hace de z -> a porque se resta, y sino el valor queda en negativo.
+     int[] freq = new int['z' - 'a' + 1]; // Se hace de z -> a porque se resta, y sino el valor queda en negativo.
         for (int i = 0; i < s.length(); i++) {
-            alphabet[s.charAt(i) - 'a']++; // El metodo charAt separa los caracteres del String y los divide en celdas, despues, dependiendo de la celda se sabe que caracter es.
+            freq[s.charAt(i) - 'a']++; // El metodo charAt separa los caracteres del String y los divide en celdas, despues, dependiendo de la celda se sabe que caracter es.
         }
                 
         int f1 = 0, f2 = 0, fc1 = 0, fc2 = 0; // f1 y f2 representan las frecuencias mas comunes en el string, fc1 y fc2 representan la cantidad de caracteres que tienen f1 y f2, por ejemplo: hay 3 A, 2 B y 3 C, f1=3| f2=2| fc1=A y C| fc2=B
         //Entiendo que puede haber mas de dos caracteres distintos, mientras tengan las misma frecuencias de f1 o f2 con respecto a los otros caracteres. de no tener la misma frecuencia, no cumple las condiciones y es un string no valido.
-        for (int i = 0; i < alphabet.length; i++) {
-            if (alphabet[i] != 0) {
-                if (alphabet[i] == f1) {
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                if (freq[i] == f1) {
                     fc1++;
-                } else if (alphabet[i] == f2) {
+                } else if (freq[i] == f2) {
                     fc2++;
                 } else if (f1 == 0) {
-                    f1 = alphabet[i];
+                    f1 = freq[i];
                     fc1++;
                 } else if (f2 == 0) {
-                    f2 = alphabet[i];
+                    f2 = freq[i];
                     fc2++;
                 } else {
                     return "NO";
