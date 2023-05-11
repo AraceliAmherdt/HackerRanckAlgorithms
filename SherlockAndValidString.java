795cdd1c65a60edb074da5f2711a6f45d721f061
AraceliAmherdt <114768217+AraceliAmherdt@users.noreply.github.com>
2023-05-11 17:46:22 -0300

    Update SherlockAndValidString.java

--- SherlockAndValidString.java
+++ SherlockAndValidString.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+    Scanner input = new Scanner(System.in);  
+      
+    String s = input.next();
+    int [] alphabet = new int ['z' - 'a' + 1];
+    for (int i = 0; i<alphabet.length; i++){
+     alphabet[s.charAt(i) - 'a']++;   
+    }
+    
+    }
+}
