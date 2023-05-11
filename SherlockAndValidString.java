1c9b49e217f945a9e9a897779905e4a09b8b7136
AraceliAmherdt <114768217+AraceliAmherdt@users.noreply.github.com>
2023-05-11 17:48:18 -0300

    Update SherlockAndValidString.java

--- SherlockAndValidString.java
+++ SherlockAndValidString.java
@@ -1,24 +1,33 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-    Scanner input = new Scanner(System.in);  
-      
-    String s = input.next();
-    int [] alphabet = new int ['z' - 'a' + 1];
-    for (int i = 0; i<alphabet.length; i++){
-     alphabet[s.charAt(i) - 'a']++;   
+public class SherlockAndValidString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(isValid(s));
     }
-    
+
+    public static String isValid(String s) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        Collection<Integer> values = frequencyMap.values();
+        int distinctValues = (int) values.stream().distinct().count();
+
+        if (distinctValues == 1) {
+            return "YES";
+        } else if (distinctValues == 2) {
+            int min = Collections.min(values);
+            int max = Collections.max(values);
+            int minCount = (int) values.stream().filter(v -> v == min).count();
+            int maxCount = (int) values.stream().filter(v -> v == max).count();
+            if ((min == 1 && minCount == 1) || (max - min == 1 && maxCount == 1)) {
+                return "YES";
+            }
+        }
+
+        return "NO";
     }
 }
